bf03037a3e99e1916b06a33083f623535ea21165
Srabana Kumar Malla <srabankumarmalla1998@gmail.com>
2023-03-01 21:33:33 +0530

    Add files via upload

---
+++ flask practise.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "WELCOME TO FLASK DEPLOYMENT FOR ML MODEL"
+
+#app.run(host='0.0.0.0', port=81)
+
+app.run(host = "127.0.0.1", port=5000)
